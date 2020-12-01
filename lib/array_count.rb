7b26844c738c6a7fe063690456e59f504e29d6fc
Esma Sherwani <esmasherr@gmail.com>
2020-12-01 17:53:51 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,7 @@
-def count_strings(array)
- array = [1, "hello", [], 5.01, "world", :name, { a: 1 }]
- array.count ("hello", "world")
-end
+#def count_strings(array)
+ #array = [1, "hello", [], 5.01, "world", :name, { a: 1 }]
+ #array.count ("hello", "world")
+#end
 
 def count_empty_strings(array)
 array = [ "", "Hello", 4, [], "", "" ]
